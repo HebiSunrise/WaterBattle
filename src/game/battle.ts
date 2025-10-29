2fd4e9b9eb6ed5607e2215f4cb87f816c8a832b5
Guzikov Vladislav <vladguzikov13@gmail.com>
2025-10-29 16:26:38 +0300

    rename and replace functions

--- src/game/battle.ts
+++ src/game/battle.ts
@@ -5,35 +5,34 @@ export enum Direction {
     VERTICAL,
 }
 
-interface ShipInfo {
+interface ShipBB {
     start: { x: number, y: number };
     end: { x: number, y: number };
 }
 
 function Battle() {
     let uid_counter = 0;
-
     let field: Field;
 
-    const ships: number[] = [];
-    const ships_lifes: { [ship: number]: number } = {};
-    const ships_info: { [ship: number]: ShipInfo } = {};
+    const ships_uids: number[] = [];
+    const ships_bb: { [ship_uid: number]: ShipBB } = {};
+    const ships_lifes: { [ship_uid: number]: number } = {};
+    const index_to_ship_uid: { [index: number]: number } = {};
 
     function setup(fieldWidth: number, fieldHiegth: number) {
         field = Field(fieldWidth, fieldHiegth);
     }
 
-    function auto_chto_to_tam() {
+    function auto_place_ships(ships: { length: number, count: number }[]) {
         // TODO: автоматически расставляем корабли
         create_ship(0, 0, 3, Direction.HORIZONTAL);
     }
 
     function create_ship(x: number, y: number, length: number, direction: Direction): number {
-        const ship = generate_uid();
-        ships.push(ship);
+        const ship_uid = generate_uid();
+        ships_uids.push(ship_uid);
 
-        ships_lifes[ship] = length;
-
+        ships_lifes[ship_uid] = length;
 
         const start = { x, y };
         let end = { x, y };
@@ -41,34 +40,34 @@ function Battle() {
         switch (direction) {
             case Direction.HORIZONTAL:
                 for (let i = 0; i < length; i++) {
-                    field.set_ship_cell(x + i, y, CellState.SHIP);
-                    field.set_cell_state(x + i, y, CellState.SHIP);
+                    index_to_ship_uid[field.get_index(x + i, y)] = ship_uid;
+                    field.set_value(x + i, y, CellState.SHIP);
                 }
                 end = { x: x + length - 1, y };
                 break;
             case Direction.VERTICAL:
                 for (let i = 0; i < length; i++) {
-                    field.set_ship_cell(x + i, y, CellState.SHIP);
-                    field.set_cell_state(x + i, y, CellState.SHIP);
+                    index_to_ship_uid[field.get_index(x + i, y)] = ship_uid;
+                    field.set_value(x + i, y, CellState.SHIP);
                 }
                 end = { x, y: y + length - 1 };
                 break;
         }
 
-        ships_info[ship] = { start, end };
+        ships_bb[ship_uid] = { start, end };
 
-        return ship;
+        return ship_uid;
     }
 
     function try_damage(x: number, y: number): boolean {
-        if (field.get_ship_cell(x, y) == -1) {
-            field.set_cell_state(x, y, CellState.MISS);
+        if (!has_ship_part(x, y)) {
+            field.set_value(x, y, CellState.MISS);
             return false;
         }
 
-        field.set_cell_state(x, y, CellState.HIT);
+        field.set_value(x, y, CellState.HIT);
 
-        const ship = field.get_ship_cell(x, y);
+        const ship = get_ship(x, y);
         ships_lifes[ship]--;
 
         if (ships_lifes[ship] == 0) {
@@ -79,27 +78,41 @@ function Battle() {
     }
 
     function miss_around_ship(ship: number) {
-        const { start, end } = ships_info[ship];
+        const { start, end } = ships_bb[ship];
         for (let y = start.y - 1; y <= end.y + 1; y++) {
             for (let x = start.x - 1; x <= end.x + 1; x++) {
                 if (field.in_boundaries(x, y)) {
-                    if (field.get_cell_state(x, y) != CellState.SHIP)
-                        field.set_cell_state(x, y, CellState.MISS);
+                    if (field.get_value(x, y) != CellState.SHIP)
+                        field.set_value(x, y, CellState.MISS);
                 }
             }
         }
     }
 
     function is_win(): boolean {
-        return ships.every(ship => ships_lifes[ship] == 0);
+        return ships_uids.every(ship => ships_lifes[ship] == 0);
+    }
+
+    function get_ship(x: number, y: number): number {
+        return index_to_ship_uid[field.get_index(x, y)];
+    }
+
+    function has_ship_part(x: number, y: number): boolean {
+        return index_to_ship_uid[field.get_index(x, y)] != undefined;
     }
 
     function generate_uid(): number {
         return uid_counter++;
     }
 
-    return { setup, auto_chto_to_tam, try_damage };
+    return { setup, auto_place_ships, try_damage };
 }
 
 const battle = Battle();
 battle.setup(10, 10);
+battle.auto_place_ships([
+    { length: 4, count: 1 },
+    { length: 3, count: 2 },
+    { length: 2, count: 3 },
+    { length: 1, count: 4 },
+]);
--- src/game/field.ts
+++ src/game/field.ts
@@ -6,44 +6,36 @@ export enum CellState {
 }
 
 export function Field(width: number, height: number) {
-
-    const cells_state: CellState[][] = [];
-    const ships_cells: number[][] = [];
+    const values: CellState[][] = [];
 
     function init() {
         for (let y = 0; y < height; y++) {
-            cells_state[y] = [];
-            ships_cells[y] = [];
+            values[y] = [];
             for (let x = 0; x < width; x++) {
-                cells_state[y][x] = CellState.EMPTY;
-                ships_cells[y][x] = -1;
+                values[y][x] = CellState.EMPTY;
             }
         }
     }
 
-    function set_cell_state(x: number, y: number, state: CellState) {
-        cells_state[y][x] = state;
+    function set_value(x: number, y: number, value: CellState) {
+        values[y][x] = value;
     }
 
-    function set_ship_cell(x: number, y: number, state: CellState) {
-        ships_cells[y][x] = state;
-    }
-
-    function get_cell_state(x: number, y: number) {
-        return cells_state[y][x];
-    }
-
-    function get_ship_cell(x: number, y: number) {
-        return ships_cells[y][x];
+    function get_value(x: number, y: number): CellState {
+        return values[y][x];
     }
 
     function in_boundaries(x: number, y: number) {
-        return y >= 0 && y < cells_state.length && x >= 0 && x < cells_state[y].length;
+        return y >= 0 && y < values.length && x >= 0 && x < values[y].length;
+    }
+
+    function get_index(x: number, y: number): number {
+        return y * width + x;
     }
 
     init();
 
-    return { set_cell_state, set_ship_cell, get_cell_state, get_ship_cell, in_boundaries };
+    return { set_value, get_value, in_boundaries, get_index, width: width, height: height };
 }
 
 export type Field = ReturnType<typeof Field>;
